76357bf6d5fe37da48a2f793d7f477aee4cc9fff
Neigevoir <neigevoir@NeigevoirdeMacBook-Pro.local>
2019-10-12 14:26:24 +0800

    fixb ug

--- src/app/desktop.js
+++ src/app/desktop.js
@@ -1,8 +1,6 @@
-import { Router, Route } from 'react-router-dom'
+import { Router } from 'react-router-dom'
 import { Provider, connect } from '../../react-redux'
-import { useEffect, useContext, useReducer } from 'react'
 import Store from 'src/app/store/store.js'
-import Layout from './router.jsx'
 import { persistor } from 'src/app/store/store.js'
 import { PersistGate } from 'redux-persist/es/integration/react'
 import Actions from 'src/app/actions/actions'
@@ -48,52 +46,6 @@ export default function First() {
       <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
         <Router history={history}>
           <Test />
-        </Router>
-      </PersistGate>
-    </Provider>
-  )
-}
-
-function App() {
-  useEffect(() => {
-    // fetch('http://localhost:8888/api/position/list', {
-    //   method: 'POST',
-    //   headers: {
-    //     Accept: 'application/json', // needed for request.format.json?
-    //     'Content-Type': 'application/json',
-    //     'X-REQUESTED-WITH': 'XMLHttpRequest'
-    //   },
-    //   // mode: 'cors',
-    //   body: JSON.stringify({
-    //     test: 1
-    //   }),
-    //   credentials: 'same-origin'
-    // }).then(response => {
-    //   if (response.status >= 200 && response.status < 400) {
-    //     // success
-    //     response
-    //       .json()
-    //       .then(data => {
-    //         // body is json
-    //         console.log(data)
-    //       })
-    //       .catch(e => {
-    //         // body is not json -> sign out does this
-    //         // errors can also get raised here from
-    //         // invalid component render in response to success
-    //         console.error(e)
-    //       })
-    //   }
-    // })
-    global.AvailWidth = window.screen.availWidth
-    global.AvailHeight = window.screen.availWidth
-  }, [])
-
-  return (
-    <Provider store={Store}>
-      <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
-        <Router history={history}>
-          <Route path="/" component={Layout} />
         </Router>
       </PersistGate>
     </Provider>
