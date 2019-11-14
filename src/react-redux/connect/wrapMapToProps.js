d5d3c8b55ea46742e6444dd73542c6b94b2b6d82
Neigevoir <luobobo@coding.net>
2019-11-14 15:10:38 +0800

    add tips

--- src/react-redux/connect/wrapMapToProps.js
+++ src/react-redux/connect/wrapMapToProps.js
@@ -21,25 +21,38 @@ export function getDependsOnOwnProps(mapToProps) {
     : mapToProps.length !== 1
 }
 
-/* 
-  TIPS：假设参数mapToProps传入的是
-  function getState(state){
-    return {
-      test: state.test
-    }
-  }
-*/
-
-// TIPS：主要是存储到mapToProps, methodName，返回一个initProxySelector的函数
+/**
+ * TIPS：主要是存储到mapToProps, methodName，返回一个initProxySelector的函数
+ * @param { mapToProps } function 用户传入的function
+ *  如：function getState(state){
+          return {
+            test: state.test
+          }
+        }
+  *  @param { methodName } string 方法名
+ */
 export function wrapMapToPropsFunc(mapToProps, methodName) {
+  /**
+   * TIPS：initProxySelector主要是拿到options的displayName，并返回一个proxy
+   * @param { dispatch } function dispatch
+   * @param { displayName } string 后面一个参数是options，一些设置的参数
+   */
   return function initProxySelector(dispatch, { displayName }) {
+    /**
+     * TIPS：proxy主要是拿到props
+     * @param { stateOrDispatch } object 一般是state，也可以是dispatch
+     * @param { ownProps } object 是组件自身拥有的props，如<MyTest text="test" />
+     * ownProps: {
+     *  text: 'test'
+     * }
+     */
     const proxy = function mapToPropsProxy(stateOrDispatch, ownProps) {
       return proxy.dependsOnOwnProps
         ? proxy.mapToProps(stateOrDispatch, ownProps)
         : proxy.mapToProps(stateOrDispatch)
     }
 
-    proxy.dependsOnOwnProps = true
+    proxy.dependsOnOwnProps = true // 在selectorFactory中的props合并就需要使用，默认需要
 
     // Tips：该函数在首次会执行
     proxy.mapToProps = function detectFactoryAndVerify(
@@ -53,11 +66,12 @@ export function wrapMapToPropsFunc(mapToProps, methodName) {
           }
         }
         赋给proxy.mapToProps，替换当前的函数
+        获取 Props { test: state.test }
       */
       proxy.mapToProps = mapToProps
       proxy.dependsOnOwnProps = getDependsOnOwnProps(mapToProps)
-      // TIPS：获取 Props { test: state.test }
       let props = proxy(stateOrDispatch, ownProps)
+
       // TIPS：如果 Props是function则继续执行该流程直到拿到Props Obejct
       if (typeof props === 'function') {
         proxy.mapToProps = props
